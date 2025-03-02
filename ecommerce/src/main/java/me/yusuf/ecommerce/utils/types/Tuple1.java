5849bfe8a4afca95206d33dcd708455cbb25117e
yozerpp <tipili-yusuf-ozer@hotmail.com>
2025-03-02 14:51:41 +0300

    feat: Add Tuple utility classes for 1-4 elements

---
+++ ecommerce/src/main/java/me/yusuf/ecommerce/utils/types/Tuple1.java
@@ -0,0 +1,29 @@
+package me.yusuf.ecommerce.utils.types;
+
+import java.util.Objects;
+
+public class Tuple1<T1> {
+    public final T1 _1;
+
+    public Tuple1(T1 _1) {
+        this._1 = _1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tuple1)) return false;
+        Tuple1<?> tuple1 = (Tuple1<?>) o;
+        return Objects.equals(_1, tuple1._1);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_1);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + _1 + ")";
+    }
+}
