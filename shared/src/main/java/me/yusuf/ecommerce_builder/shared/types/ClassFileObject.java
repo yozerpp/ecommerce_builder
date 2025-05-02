5649e65c01429cc1cfafe699fa7f90ef876ef0ee
yozerpp <tipili-yusuf-ozer@hotmail.com>
2025-05-02 10:49:54 +0300

    add Dto to ClassFileObject

--- shared/src/main/java/me/yusuf/ecommerce_builder/shared/types/ClassFileObject.java
+++ shared/src/main/java/me/yusuf/ecommerce_builder/shared/types/ClassFileObject.java
@@ -1,4 +1,6 @@
 package me.yusuf.ecommerce_builder.shared.types;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AccessLevel;
 import lombok.Getter;
 
 import javax.tools.SimpleJavaFileObject;
@@ -6,15 +8,21 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
 import java.net.URI;
+import java.util.Base64;
 
 @Getter
 public class ClassFileObject extends SimpleJavaFileObject implements Serializable{
+    @JsonIgnore
+    @Getter(AccessLevel.NONE)
     private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     private final String className;
 
 
     public byte[] getClassBytes() {
         return outputStream.toByteArray();
+    }
+    public void setClassBytes(byte[] bytes){
+        outputStream.writeBytes(bytes);
     }
     public ClassFileObject(String className, Kind kind) {
         super(URI.create("string:///" + className.replace('.', '/') + kind.extension), kind);
@@ -24,6 +32,7 @@ public class ClassFileObject extends SimpleJavaFileObject implements Serializabl
         this(className, Kind.CLASS);
         this.outputStream.writeBytes(bytes);
     }
+    @JsonIgnore
     @Override
     public OutputStream openOutputStream() {
         return outputStream;
@@ -36,8 +45,23 @@ public class ClassFileObject extends SimpleJavaFileObject implements Serializabl
         }
         return false;
     }
+    @JsonIgnore
+    public Dto toDto(){
+        return new Dto(className, Base64.getEncoder().encodeToString(getClassBytes()));
+    }
+    @JsonIgnore
+    public static ClassFileObject fromDto(Dto dto){
+        return new ClassFileObject(dto.className,Base64.getDecoder().decode(dto.content));
+    }
     @Override
     public int hashCode() {
         return getName().hashCode();
+    }
+    public static class Dto{
+        public String className, content;
+        public Dto(String className, String content){
+            this.className = className;
+            this.content = content;
+        }
     }
 }
