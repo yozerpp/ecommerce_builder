2f0d6b42c9e0f9e30be4f5b8b2ad189a75f545d3
yozerpp <tipili-yusuf-ozer@hotmail.com>
2025-05-02 10:51:35 +0300

    delete k8down and k8up

--- k8down.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl delete -f kube/config-map.yaml
-kubectl delete -f kube/secrets.yaml
-kubectl delete -f kube/demo.yaml
-kubectl delete -f kube/editor.yaml
-kubectl delete -f kube/ecommercedb.yaml
--- k8up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl apply -f kube/config-map.yaml
-kubectl apply -f kube/secrets.yaml
-kubectl apply -f kube/ecommercedb.yaml
-kubectl apply -f kube/demo.yaml
-kubectl apply -f kube/editor.yaml
